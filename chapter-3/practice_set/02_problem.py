c64b83de754e7a311d403210e3593ccb03976acf
Manmohan Singh <manmohansingh9562579@gmail.com>
2026-02-21 20:57:14 +0530

    Add files via upload

---
+++ chapter-3/practice_set/02_problem.py
@@ -0,0 +1,19 @@
+marks = []
+
+m1 = int(input("Enter marks for subject 1: ")) 
+marks.append(m1)
+m2 = int(input("Enter marks for subject 2: "))
+marks.append(m2)
+m3 = int(input("Enter marks for subject 3: "))
+marks.append(m3)
+m4 = int(input("Enter marks for subject 4: "))
+marks.append(m4)
+m5 = int(input("Enter marks for subject 5: "))
+marks.append(m5)
+m6 = int(input("Enter marks for subject 6: "))
+marks.append(m6)
+m7 = int(input("Enter marks for subject 7: "))
+marks.append(m7)
+
+marks.sort()
+print(marks)
