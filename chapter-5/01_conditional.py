8382989a11bc4b300240ce317dc24971a01638ab
Manmohan Singh <manmohansingh9562579@gmail.com>
2026-03-02 20:13:25 +0530

    Add age check for voting eligibility

---
+++ chapter-5/01_conditional.py
@@ -0,0 +1,12 @@
+a =int(input("Enter your age: ")) 
+
+# if else statement
+
+if(a>=18):
+    print("You are eligible to vote.")
+    print("You can participate in elections.")
+
+    
+else:
+    print("You are not eligible to vote.")
+    print("You have to wait until you turn 18.")
